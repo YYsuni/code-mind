aee5a63f73f70d1266a9df44fdcbb37a2d7e4ec5
Suni <yysuni1001@gmail.com>
2023-08-17 22:48:19 +0800

    Update brush panel ui

--- src/components/mind-control.tsx
+++ src/components/mind-control.tsx
@@ -2,6 +2,9 @@ import { ReactComponent as BrushSVG } from '@/svgs/brush.svg'
 import { ReactComponent as ZoomInSVG } from '@/svgs/zoom-in.svg'
 import { ReactComponent as ZoomOutSVG } from '@/svgs/zoom-out.svg'
 import { ReactComponent as CenterSVG } from '@/svgs/center.svg'
+import { ReactComponent as BoldSVG } from '@/svgs/bold.svg'
+import { ReactComponent as ItalicSVG } from '@/svgs/italic.svg'
+import { ReactComponent as StrikeThroughSVG } from '@/svgs/strike-through.svg'
 import { MAX_SCALE, MIN_SCALE, containerState, controls } from '@/share'
 import { useState } from 'react'
 
@@ -57,51 +60,99 @@ function Brush() {
 
 				{show && (
 					<div
-						className='absolute right-12 top-0 space-y-2 bg-white/80 p-3 text-xs backdrop-blur'
+						className='absolute right-12 top-0 space-y-2 rounded-lg bg-white/80 p-3 text-xs backdrop-blur'
 						style={{ width: 200 }}>
-						<div className='flex items-center gap-x-2'>
-							<span className='' style={{ width: 50 }}>
-								Width
-							</span>
-							<input type='text' className='flex-1 px-1 py-0.5' style={{ width: 60 }} />
-							<button className='border border-transparent px-1 py-0.5 hover:border-black/10'>reset</button>
+						<label className='group relative flex h-7 items-center rounded border border-transparent hover:border-black/10'>
+							<span className='absolute left-1'>Width</span>
+							<input
+								placeholder='200'
+								type='text'
+								className='h-full bg-transparent py-0.5'
+								style={{ width: 176, paddingLeft: 56, paddingRight: 60 }}
+							/>
+							<button
+								className='absolute right-0 top-0 h-full border-l border-transparent text-center hover:bg-black/5 group-hover:border-black/10'
+								style={{ width: 48 }}>
+								reset
+							</button>
+						</label>
+						<label className='group relative flex h-7 items-center rounded border border-transparent hover:border-black/10'>
+							<span className='absolute left-1'>Height</span>
+							<input
+								placeholder='48'
+								type='text'
+								className='h-full bg-transparent py-0.5'
+								style={{ width: 176, paddingLeft: 56, paddingRight: 60 }}
+							/>
+							<button
+								className='absolute right-0 top-0 h-full border-l border-transparent text-center hover:bg-black/5 group-hover:border-black/10'
+								style={{ width: 48 }}>
+								reset
+							</button>
+						</label>
+
+						<div className='border-t border-black/5' />
+
+						<div className='flex h-7 items-center justify-between gap-x-2 pl-1'>
+							<span>Fill</span>
+
+							<button className='rounded py-1 text-center hover:bg-black/5' style={{ width: 48 }}>
+								reset
+							</button>
 						</div>
-						<div className='flex items-center gap-x-2'>
-							<span className='' style={{ width: 50 }}>
-								Height
-							</span>
-							<input type='text' className='flex-1 px-1 py-0.5' style={{ width: 60 }} />
-							<button className='border border-transparent px-1 py-0.5 hover:border-black/10'>reset</button>
+						<div className='group flex h-7 items-center border border-transparent pl-1 hover:border-black/10'>
+							<div className='mr-1 h-4 w-4' style={{ backgroundColor: 'pink' }}></div>
+							<input
+								placeholder='FED2CB'
+								type='text'
+								className='h-full border-l border-r border-transparent bg-transparent px-1 py-0.5 group-hover:border-black/10'
+								style={{ width: 102 }}
+							/>
+							<input
+								type='text'
+								value='100%'
+								className='h-full bg-transparent px-1 py-0.5 text-center'
+								style={{ width: 48 }}
+							/>
 						</div>
 
-						<div className=' border-t border-black/5' />
+						<div className='border-t border-black/5' />
 
-						<div className='flex items-center justify-between gap-x-2'>
-							<span className='' style={{ width: 50 }}>
-								Fill
-							</span>
+						<div className='flex h-7 items-center justify-between gap-x-2 pl-1'>
+							<span>Text</span>
 
-							<button className='border border-transparent px-1 py-0.5 hover:border-black/10'>reset</button>
+							<button className='rounded py-1 text-center hover:bg-black/5' style={{ width: 48 }}>
+								reset
+							</button>
 						</div>
-						<div className='flex items-center gap-x-2'>
-							<div className='h-4 w-4' style={{ backgroundColor: 'pink' }}></div>
-							<input type='text' value='FED2CB' className='px-1 py-0.5' style={{ width: 60 }} />
-							<input type='text' value='100%' className='px-1 py-0.5' style={{ width: 36 }} />
+						<div className='group flex h-7 items-center border border-transparent pl-1 hover:border-black/10'>
+							<div className='mr-1 h-4 w-4' style={{ backgroundColor: 'pink' }}></div>
+							<input
+								placeholder='FED2CB'
+								type='text'
+								className='h-full border-l border-r border-transparent bg-transparent px-1 py-0.5 group-hover:border-black/10'
+								style={{ width: 102 }}
+							/>
+							<input
+								type='text'
+								value='100%'
+								className='h-full bg-transparent px-1 py-0.5 text-center'
+								style={{ width: 48 }}
+							/>
 						</div>
 
-						<div className=' border-t border-black/5' />
+						<div className='flex h-7 items-center gap-x-1'>
+							<button className='rounded p-1 text-text/60 hover:bg-black/5'>
+								<BoldSVG className='h-4 w-4' />
+							</button>
+							<button className='rounded p-1 text-text/60 hover:bg-black/5'>
+								<ItalicSVG className='h-4 w-4' />
+							</button>
+							<button className='rounded p-1 text-text/60 hover:bg-black/5'>
+								<StrikeThroughSVG className='h-4 w-4' />
+							</button>
 
-						<div className='flex items-center justify-between gap-x-2'>
-							<span className='' style={{ width: 50 }}>
-								Text
-							</span>
-
-							<button className='border border-transparent px-1 py-0.5 hover:border-black/10'>reset</button>
-						</div>
-						<div className='flex items-center gap-x-2'>
-							<div className='h-4 w-4' style={{ backgroundColor: 'pink' }}></div>
-							<input type='text' value='FED2CB' className='px-1 py-0.5' style={{ width: 60 }} />
-							<input type='text' value='100%' className='px-1 py-0.5' style={{ width: 36 }} />
+							<input className='h-full bg-transparent px-1 py-0.5' placeholder='14' style={{ width: 92 }} />
 						</div>
 					</div>
 				)}
